6731be7565f4bae13de8a74f3a9d19e2860ce92e
Marta Rodrigo <martarodrigo.55@gmail.com>
2024-06-04 16:40:47 +0200

    cambio html con clases atomicas, falta cambiar el css y aplicar Team Section tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,26 @@ module.exports = {
     './pages/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        melodrama: ['Melodrama', 'sans-serif'],
+        archivo: ['Archivo', 'sans-serif'], 
+        Tanker: ['Tanker', 'sans-serif'],
+      },
+    },
+    colors: {
+      white: '#ffffff',
+      gray: {
+        600: '#4B5563',
+        800: '#1F2937',
+      },
+      red: {
+        500: '#EF4444',
+      },
+    },
+    spacing: {
+      '4.5': '1.125rem',
+    },
   },
   plugins: [
     {
