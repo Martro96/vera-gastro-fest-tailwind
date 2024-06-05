de486912ecffef0320a25333902440f1ed210f53
Marta Rodrigo <martarodrigo.55@gmail.com>
2024-06-05 12:45:09 +0200

    Cambio html de index por el código de my Team de Tailwind pero no sé poner el estilo que quiero

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './components/**/*.{html,js}',
-    './pages/**/*.{html,js}',
-  ],
+  content: ['./*.html', './assets/scripts/*.js'],
   theme: {
     extend: {
       fontFamily: {
